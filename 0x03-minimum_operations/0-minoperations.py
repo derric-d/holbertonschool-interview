063fa878a7de8502f3a203030330fd11c6e0e767
derric-d <derric.donehoo@gmail.com>
2021-01-07 21:23:14 +0000

    prime factors problem

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python2
+"""
+function to get min steps needed to #n "h" characters
+with just copy and paste
+// = python floored division
+"""
+
+
+def minOperations(n):
+    """return num o ops needed"""
+    i = 2
+    res = 0
+
+    if (n <= 1):
+        return res
+
+    while (i <= n // 2):
+        if (n % i == 0):
+            res += i
+            n //= i
+        else:
+            i += 1
+
+    return res + n
